62e07f643c42a4d101a25bf8b7fb80859aaf9047
sandeepkasani <sandeepkasani@gmail.com>
2020-11-10 01:06:59 +0530

    colors added to fireworks

--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
 var sparks = [];
 
 function setup() {
-  createCanvas(windowWidth - 400, windowHeight-30);
-  frameRate(60);
+  createCanvas(windowWidth - 50, windowHeight-30);
+  frameRate(60);  
+  fireShot();  
+  fireShot();  
+  fireShot();  
+  fireShot();  
 }
 
 
-function burst(x,y){
+function burst(x,y, clr){
   var z = null;
-  for(let i = 0; i<50; i++){
-	z = new spark(x, y);
+  for(let i = 0; i<100; i++){
+	z = new spark(x, y, false, clr);
 	sparks.push(z)
-  }
+  }  
 }
 
 // function mouseDragged() {
-function mousePressed() {
+function mousePressed() {	
+}
+
+function fireShot() {
   let x = width/2; 
   let y = windowHeight;
   let z = new spark(x, y, true);
@@ -23,31 +30,38 @@ function mousePressed() {
 }
 
 function draw() {
-  background('rgba(0,0,0,0.1)');
+  background('rgba(0,0,0,0.2)');
   fill(255);
   noStroke();
   for(i=0; i<sparks.length;i++){
 	let s = sparks[i];
+	fill(s.scolor)
+	//s.size = random(3,10);
     ellipse(s.x, s.y, s.size, s.size);
     s.update();
     if(s.life < 0){
 	  if(s.isShot){
-		burst(s.x,s.y)
+		burst(s.x,s.y, s.scolor)
+		window.setTimeout(()=>{
+		  fireShot()
+   	    },random(1,10)*200) 
 	  }
 	  sparks.splice(i,1);
     }
   }
 }
 
-function spark(x,y,isShot) {
+function spark(x,y,isShot, pClr) {
   this.x = x;
   this.y = y;
   this.isShot = isShot;
-  this.xspeed = random(-3,3);
-  this.yspeed = isShot ? 10 : random(-2,6);
-  this.gravity = 0.1;
-  this.life = isShot ? random(40,70) :random(20,60);
-  this.size = isShot ? 10 : 5;
+  this.xspeed = random(-2,2);
+  this.yspeed = isShot ? height/60 : random(-1,5);
+  this.gravity = isShot ? 0.1 : 0.1;
+  let clrs = ['red', 'blue', 'cyan', 'magenta', 'yellow']
+  this.scolor = isShot ? clrs[Math.floor(random(0,6))]+"" : pClr;
+  this.life = isShot ? random(70,100) :random(20,60);
+  this.size = isShot ? 3 : 5;
   this.update = function(){
     this.x += this.xspeed;
     this.y -= this.yspeed;
