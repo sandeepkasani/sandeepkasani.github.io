f38275aff26d28f8b35b38523f6fa0b090a45905
sandeepkasani <sandeepkasani@gmail.com>
2020-11-27 16:36:53 +0530

    canvas video recording

--- sketch1.js
+++ sketch1.js
@@ -18,6 +18,7 @@ function preload() {
 
 function fireShots(){
   if(!started){
+	  startRecording();
 	  for(let i= 0; i<shots; i++){
 		window.setTimeout(()=>{
 		  fireShot()
@@ -27,11 +28,38 @@ function fireShots(){
   }
 }
 
+function startRecording() {
+  const chunks = []; // here we will store our recorded media chunks (Blobs)
+  let canvas = document.getElementById("mycanvas");
+  const stream = canvas.captureStream(); // grab our canvas MediaStream
+  const rec = new MediaRecorder(stream); // init the recorder
+  // every time the recorder has new data, we will store it in our array
+  rec.ondataavailable = e => chunks.push(e.data);
+  // only when the recorder stops, we construct a complete Blob from all the chunks
+  rec.onstop = e => exportVid(new Blob(chunks, {type: 'video/webm'}));
+  
+  rec.start();
+  setTimeout(()=>rec.stop(), 10000); // stop recording in 3s
+}
+
+function exportVid(blob) {
+  const vid = document.createElement('video');
+  vid.src = URL.createObjectURL(blob);
+  vid.controls = true;
+  //document.body.appendChild(vid);
+  const a = document.createElement('a');
+  a.download = 'myvid.webm';
+  a.href = vid.src;
+  a.textContent = 'download the video';
+  document.body.appendChild(a);
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.id('mycanvas');
   clrs.map((c)=>shotsData[c]={ count:0, clr: c});
   textFont(myfont);
-  frameRate(60);
+  frameRate(90);
 }
 
 
